8fc57ea5565405883fb4c24be12bc7b8a2671108
rizalmdj <rizal.muhammad.tif16@polban.ac.id>
2019-02-15 20:20:03 +0700

    konflik YANG hqq

---
+++ WpfApp1/WpfApp1/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class User
+    {
+        public string email { get; set; }
+        public string pass { get; set; }
+        public string nama { get; set; }
+    }
+}
