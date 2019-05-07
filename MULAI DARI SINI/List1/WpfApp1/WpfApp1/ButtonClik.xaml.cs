584063ab4210a236a23aead076884a96ebc1ca88
rizalmdj <rizal.muhammad.tif16@polban.ac.id>
2019-05-07 11:39:19 +0700

    Button clik

---
+++ MULAI DARI SINI/List1/WpfApp1/WpfApp1/ButtonClik.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+
+using OpenQA.Selenium.Chrome;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for ButtonClik.xaml
+    /// </summary>
+    public partial class ButtonClik : Window
+    {
+        List<Activity> Activities;
+        ChromeDriver driver;
+        MElemet data = new MElemet();
+
+        public ButtonClik(List<Activity> _Activities, ChromeDriver _driver)
+        {
+            InitializeComponent();
+            Activities = _Activities;
+            driver = _driver;
+            Console.WriteLine("Button clik");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            data.PartName = Nama.Text;
+            data.DivElement = Nama.Text;
+            Activity a = new GetData(driver, data);
+            Activities.Add(a);
+            Console.WriteLine("-----------data berhasil kesimpan-------------------");
+            Close();
+        }
+    }
+}
